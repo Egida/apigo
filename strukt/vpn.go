a7f3b572791c6240660131e891faecc9efddc3eb
sussdorf <95082790+sussdorf@users.noreply.github.com>
2024-02-15 08:39:59 +0100

    Update vpn.go
    Hhj

--- strukt/vpn.go
+++ strukt/vpn.go
@@ -1,7 +1,7 @@
 package strukt
 
 type GetAccounts struct {
-	Data interface{} {
+	Data any {
 		ID           int    `json:"id"`
 		Username     string `json:"username"`
 		Status       string `json:"status"`
