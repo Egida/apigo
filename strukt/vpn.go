dd76148f778ca2a8cb84b3447344a317251b424f
sussdorf <95082790+sussdorf@users.noreply.github.com>
2024-02-15 10:13:54 +0100

    Update vpn.go
    Update

--- strukt/vpn.go
+++ strukt/vpn.go
@@ -1,30 +1,37 @@
 package strukt
 
 type GetAccounts struct {
-	Data []struct {
-		ID           int    `json:"id"`
-		Username     string `json:"username"`
-		Status       string `json:"status"`
-		WgIP         string `json:"wg_ip"`
-		WgPrivateKey string `json:"wg_private_key"`
-		WgPublicKey  string `json:"wg_public_key"`
-		ExpiredAt    string `json:"expired_at"`
-		Updated      string `json:"updated"`
-		Created      string `json:"created"`
-	} `json:"data"`
-	Links struct {
-		First string      `json:"first"`
-		Last  string      `json:"last"`
-		Prev  interface{} `json:"prev"`
-		Next  interface{} `json:"next"`
-	} `json:"links"`
-	Meta struct {
-		CurrentPage int    `json:"current_page"`
-		From        int    `json:"from"`
-		LastPage    int    `json:"last_page"`
-		Path        string `json:"path"`
-		PerPage     int    `json:"per_page"`
-		To          int    `json:"to"`
-		Total       int    `json:"total"`
-	} `json:"meta"`
+	Data  []Item `json:"data"` 
+	Links Links   `json:"links"`
+	Meta  Meta    `json:"meta"` 
 }
+
+type Item struct {
+	ID           int64   `json:"id"`            
+	Username     string  `json:"username"`      
+	Status       string  `json:"status"`        
+	WgIP         string  `json:"wg_ip"`         
+	WgPrivateKey string  `json:"wg_private_key"`
+	WgPublicKey  string  `json:"wg_public_key"` 
+	ExpiredAt    *string `json:"expired_at"`    
+	Updated      string  `json:"updated"`       
+	Created      string  `json:"created"`       
+}
+
+type Links struct {
+	First string      `json:"first"`
+	Last  string      `json:"last"` 
+	Prev  interface{} `json:"prev"` 
+	Next  interface{} `json:"next"` 
+}
+
+type Meta struct {
+	CurrentPage int64  `json:"current_page"`
+	From        int64  `json:"from"`        
+	LastPage    int64  `json:"last_page"`   
+	Path        string `json:"path"`        
+	PerPage     int64  `json:"per_page"`    
+	To          int64  `json:"to"`          
+	Total       int64  `json:"total"`       
+}
+
