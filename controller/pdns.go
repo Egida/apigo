1b1567fe389f8e6bc52d41100e1b608fff24ff2e
Maik Sussdorf <sussdorf@dotnic.de>
2024-02-22 22:22:24 +0100

    ok

--- controller/pdns.go
+++ controller/pdns.go
@@ -10,7 +10,6 @@ import (
 
 func AddZone(c *fiber.Ctx) error {
 	var input model.AddZoneInput
-	domain := c.Params("domain")
 	if err := c.BodyParser(&input); err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
@@ -22,9 +21,7 @@ func AddZone(c *fiber.Ctx) error {
 	if input.Kind == "" {
 		input.Kind = "Master"
 	}
-	if input.Name == "" {
-		input.Name = domain + "."
-	}
+
 	if input.NameServers == nil || len(input.NameServers) == 0 {
 		input.NameServers = []string{"ns1.dnic.icu.", "ns2.dnic.icu."}
 	}
