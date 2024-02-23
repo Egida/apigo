8d9d270f2d251fe88b5c8028aaa7341218157f87
sussdorf <95082790+sussdorf@users.noreply.github.com>
2024-02-23 12:59:22 +0100

    Update zones.go

--- pdns/zones.go
+++ pdns/zones.go
@@ -66,7 +66,7 @@ func GetZone(zoneID string) (ZoneCreate, error) {
 		return ZoneCreate{}, err
 	}
 
-	var response ZoneCreate
+	var response []ZoneCreate
 	err = jsoniter.Unmarshal(body, &response)
 	if err != nil {
 		return ZoneCreate{}, err
@@ -75,7 +75,7 @@ func GetZone(zoneID string) (ZoneCreate, error) {
 	return response, nil
 }
 
-func Add(input model.AddZoneInput) (*ZoneCreate, error) {
+func Add(input model.AddZoneInput) ([]ZoneCreate, error) {
 	body, err := client.post("servers/localhost/zones", input)
 	if err != nil {
 		fmt.Println("error_body:", string(body))
