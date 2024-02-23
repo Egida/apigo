0c09d8c25fbd1efbbbe96bd6aebc6fcc75c15a36
sussdorf <95082790+sussdorf@users.noreply.github.com>
2024-02-23 14:29:53 +0100

    Update zones.go

--- pdns/zones.go
+++ pdns/zones.go
@@ -75,7 +75,7 @@ func GetZone(zoneID string) (ZoneCreate, error) {
 	return response, nil
 }
 
-func Add(input model.AddZoneInput) ([]ZoneCreate, error) {
+func Add(input model.AddZoneInput) (*ZoneCreate, error) {
 	body, err := client.post("servers/localhost/zones", input)
 	if err != nil {
 		fmt.Println("error_body:", string(body))
