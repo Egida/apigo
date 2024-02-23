8c7ad6f6c37503e056e19ea186e16010fdb10ea1
sussdorf <95082790+sussdorf@users.noreply.github.com>
2024-02-23 07:15:42 +0100

    Update zones.go

--- pdns/zones.go
+++ pdns/zones.go
@@ -32,6 +32,40 @@ type Zone struct {
 	MasterTSIGKeyIDs []string `json:"master_tsig_key_ids,omitempty"`
 	SlaveTSIGKeyIDs  []string `json:"slave_tsig_key_ids,omitempty"`
 }
+type ZoneCreate struct {
+	Account          string        `json:"account"`            
+	APIRectify       bool          `json:"api_rectify"`        
+	Dnssec           bool          `json:"dnssec"`             
+	EditedSerial     int64         `json:"edited_serial"`      
+	ID               string        `json:"id"`                 
+	Kind             string        `json:"kind"`               
+	LastCheck        int64         `json:"last_check"`         
+	MasterTsigKeyIDS []interface{} `json:"master_tsig_key_ids"`
+	Masters          []interface{} `json:"masters"`            
+	Name             string        `json:"name"`               
+	NotifiedSerial   int64         `json:"notified_serial"`    
+	Nsec3Narrow      bool          `json:"nsec3narrow"`        
+	Nsec3Param       string        `json:"nsec3param"`         
+	Rrsets           []Rrset       `json:"rrsets"`             
+	Serial           int64         `json:"serial"`             
+	SlaveTsigKeyIDS  []interface{} `json:"slave_tsig_key_ids"` 
+	SOAEdit          string        `json:"soa_edit"`           
+	SOAEditAPI       string        `json:"soa_edit_api"`       
+	URL              string        `json:"url"`                
+}
+
+type Rrset struct {
+	Comments []interface{} `json:"comments"`
+	Name     string        `json:"name"`    
+	Records  []Record      `json:"records"` 
+	TTL      int64         `json:"ttl"`     
+	Type     string        `json:"type"`    
+}
+
+type Record struct {
+	Content  string `json:"content"` 
+	Disabled bool   `json:"disabled"`
+}
 
 func ListZones() ([]Zone, error) {
 	body, err := client.get("/servers/localhost/zones")
@@ -65,17 +99,17 @@ func GetZone(zoneID string) (Zone, error) {
 	return response, nil
 }
 
-func Add(input model.AddZoneInput) (Zone, error) {
+func Add(input model.AddZoneInput) (ZoneCreate, error) {
 	body, err := client.post("servers/localhost/zones", input)
 	if err != nil {
 		fmt.Println("error_body:", string(body))
-		return Zone{}, err
+		return ZoneCreate{}, err
 	}
 
 	var response Zone
 	err = jsoniter.Unmarshal(body, &response)
 	if err != nil {
-		return Zone{}, err
+		return ZoneCreate{}, err
 	}
 
 	return response, nil
